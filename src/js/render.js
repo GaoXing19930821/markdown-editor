99b6b57792f787e5dc27fd101a06bf26c69ffd4c
高兴01 <gaoxing01@ESG.360ES.CN>
2018-07-23 21:33:12 +0800

    complete

--- src/js/render.js
+++ src/js/render.js
@@ -21,25 +21,62 @@ const filter = {
 class Render{
   constructor(markdown){
     this.markdown = markdown;
-    this.markdownArr = this.getArray()
+    this.markdownArr = this.getArray();
+    this.inline = this.inlinerender();
   }
   inlinerender(){
-    let output = [] , match, content;
-    const mdArr = this.markdownArr;
-    for(let i=0;i<=mdArr.length;i++){
-      if(match=filter.inline.bold.exec(mdArr[i])){
-        content = '<strong>' + match[1] + '</strong>';
-      }
-      else if(match=filter.inline.italic.exec(mdArr[i])){
-        content = '<em>' + match[1] + '</em>';
-      }
-      else if(match=filter.inline.code.exec(mdArr[i])){
-        content = '<code>' + match[1] + '</code>';
-      }
+    let output = [] , content;
 
+    let str = "**Marked** - Markdown Parser\n========================\n[Marked] lets you convert [Markdown] into HTML. Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML. This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.";
+    let mdArr = str.split("\n");
+    console.log(mdArr);
+    // const mdArr = this.markdownArr;
+    for(let i=0;i<=mdArr.length-1;i++){
+      if(filter.inline.bold.test(mdArr[i])){
+        content = this.addInlineLabel(filter.inline.bold,mdArr[i],'strong');
+      }
+      else if(filter.inline.italic.test(mdArr[i])){
+        content = this.addInlineLabel(filter.inline.italic,mdArr[i],'em');
+      }
+      else if(filter.inline.code.test(mdArr[i])){
+        content = this.addInlineLabel(filter.inline.code,mdArr[i],'code');
+      }else{
+        content = mdArr[i];
+      }
+      output.push(content);
     }
+
     return output;
   };
+  render(){
+    //console.log(this.markdownArr);
+    let output = [] ,content;
+    const inlineArr = this.inline;
+    for(let i=0;i<inlineArr.length;i++){
+      content = '<p>'+ inlineArr[i] + '</p>\n';
+      output.push(content);
+    }
+
+    return output;
+  }
+
+  addInlineLabel(reg,content,label){
+    let result = '';
+    let addlabel = [];
+    const match = content.match(reg);
+    for(let i=0;i<=match.length-1;i++){
+      let labelplus = '<' + label + '>' + reg.exec(match[i])[1] + '</' + label + '>';
+     addlabel.push(labelplus);
+    }
+
+    const rest = content.split(/(?:\*|_){2}(.+?)(?:\*|_){2}/g);
+    console.log(rest);
+    for(let i=0;i<=rest.length-1;i++){
+     result = result + rest[i] + addlabel[i];
+     console.log(result)
+    }
+    return result;
+  }
   getArray(){
     return this.markdown.split('\n');
   }
